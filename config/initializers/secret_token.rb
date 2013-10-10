f65a177567d9606a83edd36a2e5e3cc09d258c3c
lintaho <linhao.zhang@gmail.com>
2013-10-09 21:25:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Subloom::Application.config.secret_key_base = '5da682489e2ae7a34c7ebe41d9d805b6179a716879618f4305db9024d9a9c98ac205a8f4f1cd2251e363298698f7133b1b96852dd58c83585c47249b9a8e9b1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Subloom::Application.config.session_store :cookie_store, key: '_subloom_session'
